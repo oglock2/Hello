3d9567e31b2b8f9abf7026be051de90bf28da5aa
Olivia Glock <glockolivia@gmail.com>
2019-08-29 08:43:22 -0500

    added line that prints major
    marketing and computer science

--- hello.c
+++ hello.c
@@ -4,5 +4,9 @@
 int main(int arfc, char **argv) {
 
   printf("Hello World!\n") ;
+  printf("Olivia Glock\n") ;
+  printf("Marketing\n") ;
+  printf("Olivia Prawoto\n") ;
+  printf("Computer Science\n") ;
   return 0;
 }
